1dae2990e77ada21e18a6cc9830b3530174c490e
Padam Damai <padamdamai888@gmail.com>
2023-12-24 16:00:37 +0545

    added slide setting for card components

---
+++ src/utils/comman.jsx
@@ -0,0 +1,18 @@
+export const slideSettings ={
+    slidesPerView : 1,
+    spaceBetween : 30,
+    breakpoints : {
+        400: {
+            slidesPerView: 1
+        },
+        600: {
+            slidesPerView: 2
+        },
+        750: {
+            slidesPerView: 3
+        },
+        1180: {
+            slidesPerView: 4
+        }
+    }
+}
