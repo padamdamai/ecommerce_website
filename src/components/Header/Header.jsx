9bfb223cf37c48e4e8f208bdb5a0c324695baf91
Padam Damai <padamdamai888@gmail.com>
2024-01-02 23:02:01 +0545

    dropdownmenu added for small deive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Header.css';
+import {BiMenuAltRight}  from 'react-icons/bi'
 const Header = () => {
+	const [menuOpen ,setMenuOpen] = React.useState(false)
+	const getMenuStyles = (menuOpen)=>{
+		if(document.documentElement.clientWidth <= 800)
+			{
+			return {left :!menuOpen && "-100%"}
+		}
+	}
 	return (
 			<section className="h-wrapper">
 				<div className="flexCenter paddings h-container">
 					 <div className="logo">zyrundo</div>
 
-				<div className="flexCenter h-menu">
+				<div className="flexCenter h-menu h_menu2" 
+				style={getMenuStyles(menuOpen)}>
 					<a href=""> Residencies</a>
 					<a href="">Our value </a>
 					<a href=""> Contact us</a>
@@ -15,6 +24,10 @@ const Header = () => {
 						<a href=""> Contact</a>
 					</button>
 				</div>
+				</div>
+				<div className="menu_icon"
+				onClick={()=>setMenuOpen((previous)=>!previous)}>
+					<BiMenuAltRight size={30}/>
 				</div>
 			</section>
 		)
